bc891ea15606184acfbfa611f0f0bd35fa6ff6bf
Carla Boggio <cboggio@dappsfactory.io>
2025-05-16 13:25:17 -0300

    add

--- src/components/Chains.tsx
+++ src/components/Chains.tsx
@@ -3,7 +3,7 @@ import { Chain } from '@rainbow-me/rainbowkit'
 export const megaEth = {
     id: 6342,
     name: 'MEGA Testnet',
-    iconUrl: 'https://img.cryptorank.io/coins/mega_eth1736756550892.png', // Asumiendo una URL del icono, reemplázala con la URL correcta
+    iconUrl: 'https://img.cryptorank.io/coins/mega_eth1736756550892.png',
     iconBackground: '#fff',
     nativeCurrency: { name: 'MEGA Testnet Ether', symbol: 'ETH', decimals: 18 },
     rpcUrls: {
@@ -14,7 +14,7 @@ export const megaEth = {
     },
     contracts: {
       multicall3: {
-        address: '0xcA11bde05977b3631167028862bE2a173976CA11', // Usando la misma dirección que en el ejemplo, reemplázala si es diferente
+        address: '0xca11bde05977b3631167028862be2a173976ca11', // Usando la misma dirección que en el ejemplo, reemplázala si es diferente
         blockCreated: 1, // Asumiendo que es el bloque inicial, ajústalo según sea necesario
       },
     },
